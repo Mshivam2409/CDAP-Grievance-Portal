b7a75e223fb57c1846a21262d37e4447ac5f83bd
Mshivam2409 <shivammalhotra08@gmail.com>
2020-08-22 17:16:09 +0530

    Added .env generation and db

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 rm -rf build/
 mkdir -p build/web
+mkdir -p build/database
 
 cd client
 yarn run build
@@ -9,8 +10,17 @@ rm -rf build/
 cd ../server
 yarn run tsc
 cp -a  build/* ../build
+cp -a  src/data/* ../build/database
 cp -a  package.json ../build
 rm -rf build/
 
 cd ../build
 yarn install --prod
+touch .env
+cat>>.env<<"EOF"
+NODE_ENV=production
+DIR=.
+NODE_PATH=./
+PORT=8000
+BUILD_DIRECTORY=web
+EOF
